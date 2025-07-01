0643beee4dae83f330357abcde021921196ce393
Sungjun Park <espoir618@gmail.com>
2025-07-02 00:09:52 +0900

    Edit code to use timeline

--- src/pages/Test.ts
+++ src/pages/Test.ts
@@ -14,15 +14,22 @@ export const createTest = (): HTMLElement => {
   container.appendChild(box);
 
   // GSAP animation
-  gsap.to(box, {
-    duration: 1,
-    x: 200,
-    rotation: 1000,
-    scale: 3,
-    ease: "power2.inOut",
-    repeat: -1, // infinite repeat
-    yoyo: true,
-  });
+  // gsap.to(box, {
+  //   duration: 1,
+  //   x: 200,
+  //   rotation: 1000,
+  //   scale: 3,
+  //   ease: "power2.inOut",
+  //   repeat: -1, // infinite repeat
+  //   yoyo: true,
+  // });
+
+  // use timeline
+  const tl = gsap.timeline({ repeat: -1, yoyo: true });
+
+  tl.to(box, { x: 100, duration: 1 }) // 0s~1s
+    .to(box, { y: 100, duration: 1 }) // 1s~2s
+    .to(box, { scale: 1.5, duration: 1 }); // 2s~3s
 
   return container;
 };
