e7964c9f64a4ff8c6bc8a82b8f0e4f3674e5d612
Sungjun Park <espoir618@gmail.com>
2025-07-09 17:30:44 +0900

    Edit Triangle.ts

--- src/pages/Triangle.ts
+++ src/pages/Triangle.ts
@@ -2,6 +2,52 @@ import { gsap } from "gsap";
 import { createButton, createDiv } from "../utils/dom";
 import { GSDevTools } from "gsap/GSDevTools";
 
+class Triangle {
+  id: string;
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+  x3: number;
+  y3: number;
+  fill: string;
+
+  constructor(
+    id: string,
+    x1: number,
+    y1: number,
+    x2: number,
+    y2: number,
+    x3: number,
+    y3: number,
+    fill: string
+  ) {
+    this.id = id;
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+    this.x3 = x3;
+    this.y3 = y3;
+    this.fill = fill;
+  }
+
+  draw(svgContainer: HTMLElement): void {
+    const polygon = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "polygon"
+    );
+    polygon.setAttribute("id", this.id);
+    polygon.setAttribute("class", "trg");
+    polygon.setAttribute(
+      "points",
+      `${this.x1},${this.y1} ${this.x2},${this.y2} ${this.x3},${this.y3} ${this.x1},${this.y1}`
+    );
+    polygon.setAttribute("fill", this.fill);
+    svgContainer.appendChild(polygon);
+  }
+}
+
 // set style [id:container]
 const containerClass =
   "w-full h-screen flex flex-col items-center justify-center bg-white";
