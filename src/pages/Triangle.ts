a5c9877459bca8b218a9b00f1e3be6748cea52dd
Sungjun Park <espoir618@gmail.com>
2025-07-12 17:45:36 +0900

    Edit Triangle.ts

--- src/pages/Triangle.ts
+++ src/pages/Triangle.ts
@@ -1,51 +1,31 @@
 import { gsap } from "gsap";
 import { createDiv } from "../utils/dom";
 
-class Triangle {
-  id: string;
-  x1: number;
-  y1: number;
-  x2: number;
-  y2: number;
-  x3: number;
-  y3: number;
-  fill: string;
-
-  constructor(
-    id: string,
-    x1: number,
-    y1: number,
-    x2: number,
-    y2: number,
-    x3: number,
-    y3: number,
-    fill: string
-  ) {
-    this.id = id;
-    this.x1 = x1;
-    this.y1 = y1;
-    this.x2 = x2;
-    this.y2 = y2;
-    this.x3 = x3;
-    this.y3 = y3;
-    this.fill = fill;
-  }
-
-  draw(svgContainer: HTMLElement): void {
-    const polygon = document.createElementNS(
-      "http://www.w3.org/2000/svg",
-      "polygon"
-    );
-    polygon.setAttribute("id", this.id);
-    polygon.setAttribute("class", "trg");
-    polygon.setAttribute(
-      "points",
-      `${this.x1},${this.y1} ${this.x2},${this.y2} ${this.x3},${this.y3} ${this.x1},${this.y1}`
-    );
-    polygon.setAttribute("fill", this.fill);
-    svgContainer.appendChild(polygon);
-  }
-}
+const drawTriangle = (
+  id: string,
+  x1: number,
+  y1: number,
+  x2: number,
+  y2: number,
+  x3: number,
+  y3: number,
+  fill: string,
+  svgContainer: HTMLElement
+): SVGPolygonElement => {
+  const polygon = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "polygon"
+  );
+  polygon.setAttribute("id", id);
+  polygon.setAttribute("class", "trg");
+  polygon.setAttribute(
+    "points",
+    `${x1},${y1} ${x2},${y2} ${x3},${y3} ${x1},${y1}`
+  );
+  polygon.setAttribute("fill", fill);
+  svgContainer.appendChild(polygon);
+  return polygon;
+};
 
 // set style [id:container]
 const containerClass =
@@ -55,24 +35,24 @@ export const createTriangle = (): HTMLElement => {
   const container = createDiv("container", containerClass);
   const svgContainer = createDiv("svgContainer", "");
 
-  let y1 = 60.6,
-    y2 = 70.7,
-    y3 = 70.7;
-  const quantRow = 15,
-    quantColumn = 27;
-  const stepX = 30,
-    stepY = 30;
+  let y1 = 60.6;
+  let y2 = 70.7;
+  let y3 = 70.7;
+  const quantRow = 15;
+  const quantColumn = 27;
+  const stepX = 30;
+  const stepY = 30;
   let i = 0;
 
   for (let j = 0; j < quantRow; j++) {
-    let x1 = 77.9,
-      x2 = 61.7,
-      x3 = 94;
+    let x1 = 77.9;
+    let x2 = 61.7;
+    let x3 = 94;
     let angle = -90;
 
     for (let k = 0; k < quantColumn; k++) {
       i++;
-      const triangle = new Triangle(
+      const triangle = drawTriangle(
         `trg${i}`,
         x1,
         y1,
@@ -80,11 +60,11 @@ export const createTriangle = (): HTMLElement => {
         y2,
         x3,
         y3,
-        "yellow"
+        "yellow",
+        svgContainer
       );
-      triangle.draw(svgContainer);
 
-      gsap.set(`#trg${i}`, {
+      gsap.set(triangle, {
         transformOrigin: "-50% -50%",
         rotate: angle,
       });
